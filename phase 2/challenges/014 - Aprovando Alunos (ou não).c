7efab5df546a839d9ed1bdfe1aa9f6e7d9641af4
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-08-24 12:51:18 -0300

    c014 add

---
+++ phase 2/challenges/014 - Aprovando Alunos (ou não).c
@@ -0,0 +1,8 @@
+/*
+014 - Aprovando Alunos (ou não)
+
+Escreva uma função que receba 4 notas e retorne um valor verdade (0 ou 1),
+de forma que identifique se o aluno está aprovado ou não. Considere
+a média de corte 7,0.
+*/
+
