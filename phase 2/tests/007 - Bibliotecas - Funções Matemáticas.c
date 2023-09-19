39b6930cc3dca6044c5091b44ffc668c1207073c
Ícaro Pavan <icaro.pavan97@gmail.com>
2023-09-19 12:14:34 -0300

    T007 update

--- phase 2/tests/007 - Bibliotecas - Funções Matemáticas.c
+++ phase 2/tests/007 - Bibliotecas - Funções Matemáticas.c
@@ -28,15 +28,30 @@ void menu(float num1, float num2)
 {
 	int answer ;
 	
-	printf("[1] Somar\n[2] Subtrair\n[3] Multiplicar\n[4] Dividir\n[5] Exponenciar\n");
+	printf("\n[1] Somar\n[2] Subtrair\n[3] Multiplicar\n[4] Dividir\n[5] Exponenciar\n");
 	scanf("%i", &answer);
+	
+	printf("\n");
 	
 	switch(answer)
 	{
 		case 1:
-			
-			printf("%f + %f = %f", num1, num2, somar(num1, num2));
-			
+			printf("%.3f + %.3f = %.3f\n", num1, num2, somar(num1, num2));
+			break;
+		case 2:
+			printf("%.3f - %.3f = %.3f\n", num1, num2, subtrair(num1, num2));
+			break;
+		case 3:
+			printf("%.3f . %.3f = %.3f\n", num1, num2, multiplicar(num1, num2));
+			break;
+		case 4:
+			if(dividir(num1, num2) == -1)
+				printf("Erro. Divisor não pode ser zero (0).\n");
+			else
+				printf("%.3f / %.3f = %.3f\n", num1, num2, dividir(num1, num2));
+			break;
+		case 5:
+			printf("%.3f ^ %.3f = %.3f\n", num1, num2, exponenciar(num1, num2));
 			break;
 	}
 }
--- phase 2/tests/FuncoesMatematicas.h
+++ phase 2/tests/FuncoesMatematicas.h
@@ -1,12 +1,36 @@
 // Header: Funções Matemáticas
 
+#include <math.h>
+
 #ifndef FUNCOESMAT_H
 #define FUNCOESMAT_H
 
-float somar(float num1, float num2);
-float subtrair(float num1, float num2);
-float multiplicar(float num1, float num2);
-float dividir(float num1, float num2);
-float exponenciar(float num1, float num2);
+float somar(float num1, float num2)
+{
+	return num1 + num2 ;
+}
+
+float subtrair(float num1, float num2)
+{
+	return num1 - num2 ;
+}
+
+float multiplicar(float num1, float num2)
+{
+	return num1 * num2 ;
+}
+
+float dividir(float num1, float num2)
+{
+	if(num2 == 0)
+		return -1 ;
+	else
+		return num1 / num2 ;
+}
+
+float exponenciar(float num1, float num2)
+{
+	return pow(num1, num2);
+}
 
 #endif
